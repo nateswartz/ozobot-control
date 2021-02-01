64301cc41b84702d0cab3c747826fa733baf1638
nswartzentruber <nate.swartzentruber@listrak.com>
2021-02-01 11:22:15 -0500

    Refactor program to allow user input

--- OzobotControl/Program.cs
+++ OzobotControl/Program.cs
@@ -8,60 +8,72 @@ namespace OzobotControl
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var adapter = new WinRTBluetoothAdapter();
-
+            BluetoothKernel kernel = null;
             adapter.Discover(async info =>
             {
                 Console.WriteLine($"Discovered device: {info.Name}");
-                if (!info.Name.StartsWith("Ozo"))
+                if (!info.Name.StartsWith("Ozo") || kernel != null)
                     return;
-                var kernel = new BluetoothKernel(adapter, BluetoothConstants.OzobotService, BluetoothConstants.OzobotControlCharacteristic, null)
+                kernel = new BluetoothKernel(adapter, BluetoothConstants.OzobotService, BluetoothConstants.OzobotControlCharacteristic, null)
                 {
                     BluetoothAddress = info.BluetoothAddress
                 };
-                await kernel.ConnectAsync();
-                while (!kernel.IsConnected)
-                {
-                    Console.WriteLine("Waiting...");
-                }
-                var stopCommand = "50-02-01";
-
-                // Control all lights
-                var lightOff = "44-FF-FF-00-00-00";
-                var lightOnFull = "44-FF-FF-37-35-FF";
-                var lightRed = "44-FF-FF-37-00-00";
-
-                // Control 3 big lights
-                //var somethingElse = "44-2b-00-00-FF-FF";
-                var something = "44-2b-00-37-00-00";
-
-                // Control 2 smaller lights
-                //var somethingElse = "44-15-00-00-FF-FF";
-
-                // Control just the top light
-                //var somethingElse = "44-01-07-FF-60-00";
-
-                // Control everything except the top light
-                var somethingElse = "44-3E-07-FF-60-00";
-
-                await kernel.SendBytesAsync(BytesStringUtil.StringToData(stopCommand));
-                await kernel.SendBytesAsync(BytesStringUtil.StringToData(lightRed));
-                await Task.Delay(1000);
-                await kernel.SendBytesAsync(BytesStringUtil.StringToData(lightOnFull));
-                await Task.Delay(1000);
-                await kernel.SendBytesAsync(BytesStringUtil.StringToData(lightOff));
-                await Task.Delay(2000);
-                await kernel.SendBytesAsync(BytesStringUtil.StringToData(somethingElse));
-                await Task.Delay(2000);
-                await kernel.SendBytesAsync(BytesStringUtil.StringToData(lightOff));
-                await Task.Delay(2000);
-                await kernel.SendBytesAsync(BytesStringUtil.StringToData(something));
-                Console.WriteLine("Sent command");
+                await Task.CompletedTask;
             });
-            Console.WriteLine("Press Enter to cancel");
-            Console.ReadLine();
+
+            while (kernel == null)
+            {
+                await Task.Delay(10);
+            }
+
+            await kernel.ConnectAsync();
+            Console.WriteLine("Connecting...");
+            while (!kernel.IsConnected)
+            {
+                await Task.Delay(10);
+            }
+            var stopCommand = "50-02-01";
+            await kernel.SendBytesAsync(BytesStringUtil.StringToData(stopCommand));
+
+            Console.WriteLine("Enter a command (i.e. 44-FF-FF-37-35-FF):");
+            var command = Console.ReadLine();
+            await kernel.SendBytesAsync(BytesStringUtil.StringToData(command));
+
+            //// Control all lights
+            //var lightOff = "44-FF-FF-00-00-00";
+            //var lightOnFull = "44-FF-FF-37-35-FF";
+            //var lightRed = "44-FF-FF-37-00-00";
+
+            //// Control 3 big lights
+            ////var somethingElse = "44-2b-00-00-FF-FF";
+            //var something = "44-2b-00-37-00-00";
+
+            //// Control 2 smaller lights
+            ////var somethingElse = "44-15-00-00-FF-FF";
+
+            //// Control just the top light
+            ////var somethingElse = "44-01-07-FF-60-00";
+
+            //// Control everything except the top light
+            //var somethingElse = "44-3E-07-FF-60-00";
+
+            //await kernel.SendBytesAsync(BytesStringUtil.StringToData(lightRed));
+            //await Task.Delay(1000);
+            //await kernel.SendBytesAsync(BytesStringUtil.StringToData(lightOnFull));
+            //await Task.Delay(1000);
+            //await kernel.SendBytesAsync(BytesStringUtil.StringToData(lightOff));
+            //await Task.Delay(2000);
+            //await kernel.SendBytesAsync(BytesStringUtil.StringToData(somethingElse));
+            //await Task.Delay(2000);
+            //await kernel.SendBytesAsync(BytesStringUtil.StringToData(lightOff));
+            //await Task.Delay(2000);
+            //await kernel.SendBytesAsync(BytesStringUtil.StringToData(something));
+            //Console.WriteLine("Sent command");
+            //Console.WriteLine("Press Enter to cancel");
+            //Console.ReadLine();
 
             Console.WriteLine("Exiting");
         }
